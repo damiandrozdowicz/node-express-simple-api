2ea5d0d34397c4f5810cf261270a45a6442e10c3
damiandrozdowicz <info@damiandrozdowicz.com>
2025-04-05 13:03:49 +0200

    init

---
+++ src/models/contact.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+export const contactSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    company: {
+        type: String
+    },
+    phone: {
+        type: Number
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    }
+});
